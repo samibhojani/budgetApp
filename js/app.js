45b536ea9778bf50b3256d7076f6a7b065ec9b5a
Sami Bhojani <65404958+samibhojani@users.noreply.github.com>
2023-06-03 13:52:36 +0500

    adding functionality of GetBudget and GetExpense
    Balance is not done yet

--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+
+// Add budget in summary
+
 function getBudget() {
     const addBudget = document.getElementById("addBudget").value;
     document.getElementById("displayBudget").innerHTML = addBudget;
@@ -6,9 +9,52 @@ function getBudget() {
 document.getElementById("setBudgetBtn").onclick = getBudget
 
 
+
+
+// Add new expense + record previous + add expense in entryTable
+
+
+let arrOfTableEntries = []
+
 function getExpense() {
-    const addExpense = document.getElementById("expenseAmount").value;
-    document.getElementById("totalExpense").innerHTML = getExpense;
+    const expDate = document.getElementById("expenseDate").value
+    const expType = document.getElementById("expenseType").value
+    const expDesc = document.getElementById("descriptionForExpense").value
+    const expAmount = +document.getElementById("expenseAmount").value
+    const expenseBox = document.getElementById("totalExpense")
+    const expTableEntries = document.getElementById("expTableEntries")
+    const balanceAmount = document.getElementById("balanceAmount")
+
+    expenseBox.innerText = +expenseBox.innerText + expAmount
+    
+    arrOfTableEntries.push({
+        expDate,
+        expType,
+        expDesc,
+        expAmount
+    })
+
+
+    expTableEntries.innerHTML += `
+
+    <tr>
+    <td>${expDate}</td>
+    <td>${expType}</td>
+    <td>${expDesc}</td>
+    <td>${expAmount}</td>
+
+
+    <td><button id="editEntryBtn">Edit</button></td>
+    <td><button id="deleteEntryBtn">Delete</button></td>
+
+    </tr>
+    
+    `
+
+
+
+    // Calculating Balance will add later   
+
 }
 
 document.getElementById("addExpenseBtn").onclick = getExpense
